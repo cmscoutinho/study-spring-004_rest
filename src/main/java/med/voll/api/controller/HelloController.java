911b9500d64ffe48593af1ba89d559576a872c18
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-17 15:38:36 -0300

    refactor: annoteate HelloController

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -1,2 +1,9 @@
-package med.voll.api.controller;public class HelloController {
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
 }
