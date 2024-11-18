70e1511ebaa2b8196fe508122546d333a941f921
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-17 22:49:49 -0300

    refactor: define prototype method register()

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,9 +1,13 @@
 package med.voll.api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/doctors")
 public class DoctorController {
+
+    @PostMapping
+    public void register() {};
 }
