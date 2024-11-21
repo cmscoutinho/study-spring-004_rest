ef3a315fc3f3baf4d2c08fcf1cccdee6e49a3ca1
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-21 17:15:39 -0300

    feat: create record Address

--- src/main/java/med/voll/api/model/doctor/DoctorRegistryData.java
+++ src/main/java/med/voll/api/model/doctor/DoctorRegistryData.java
@@ -1,3 +1,5 @@
 package med.voll.api.model.doctor;
 
+import med.voll.api.model.Address;
+
 public record DoctorRegistryData (String name, String email, String crm, Speciality speciality, Address address) {}
