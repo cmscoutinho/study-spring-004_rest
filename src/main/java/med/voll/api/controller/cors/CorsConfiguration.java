c0d777177395da90baad0815649a6eb1748218cf
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-20 22:10:46 -0300

    feat: implement CORS configuration

--- src/main/java/med/voll/api/controller/cors/CorsConfiguration.java
+++ src/main/java/med/voll/api/controller/cors/CorsConfiguration.java
@@ -1,4 +1,12 @@
 package med.voll.api.controller.cors;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
 public class CorsConfiguration {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
 }
