b917a1a2987b36e51e9e897bd1812301a0429b22
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-25 13:03:17 -0300

    feat: make dependecy injection on Doctor controller's repository

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,6 +1,8 @@
 package med.voll.api.controller;
 
 import med.voll.api.model.doctor.DoctorRegistryData;
+import med.voll.api.model.doctor.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/doctors")
 public class DoctorController {
 
+    @Autowired
+    private DoctorRepository repository;
     @PostMapping
     public void register(@RequestBody DoctorRegistryData data) {
         System.out.println(data);
