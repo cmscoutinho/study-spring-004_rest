1b51c7c8ae84334c9ce7bb820eb3c70c1c068d88
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-25 12:58:06 -0300

    refactor: annotate Doctor with lombok

--- src/main/java/med/voll/api/model/doctor/Doctor.java
+++ src/main/java/med/voll/api/model/doctor/Doctor.java
@@ -1,10 +1,18 @@
 package med.voll.api.model.doctor;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import med.voll.api.model.address.AddressData;
 
 @Entity(name = "Doctor")
 @Table(name = "doctors")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Doctor {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
