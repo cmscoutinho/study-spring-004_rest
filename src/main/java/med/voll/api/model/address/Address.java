f27dd481e17a7bb95ae19f0e7e3ef93dc1b228d5
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-25 12:46:17 -0300

    feat: define model Address fields

--- src/main/java/med/voll/api/model/address/Address.java
+++ src/main/java/med/voll/api/model/address/Address.java
@@ -1,4 +1,12 @@
 package med.voll.api.model.address;
 
 public class Address {
+
+    private String street;
+    private String neighbourhood;
+    private String zipCode;
+    private String city;
+    private String state;
+    private String additionalInfo;
+    private String number;
 }
