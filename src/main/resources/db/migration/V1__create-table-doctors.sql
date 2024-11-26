a18dfce59b6956168f9a6446a4e79601fa6656bc
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-11-26 15:37:20 -0300

    boot: translate flyway migration file

--- src/main/resources/db/migration/V1__create-table-doctors.sql
+++ src/main/resources/db/migration/V1__create-table-doctors.sql
@@ -1,17 +1,17 @@
-create table medicos(
+create table doctors(
 
     id bigint not null auto_increment,
-    nome varchar(100) not null,
+    name varchar(100) not null,
     email varchar(100) not null unique,
     crm varchar(6) not null unique,
-    especialidade varchar(100) not null,
-    logradouro varchar(100) not null,
-    bairro varchar(100) not null,
-    cep varchar(9) not null,
-    complemento varchar(100),
-    numero varchar(20),
-    uf char(2) not null,
-    cidade varchar(100) not null,
+    speciality varchar(100) not null,
+    street varchar(100) not null,
+    neighbourhood varchar(100) not null,
+    zipCode varchar(9) not null,
+    additionalInfo varchar(100),
+    number varchar(20),
+    state char(2) not null,
+    city varchar(100) not null,
 
     primary key(id)
 
