c0395b3120a02b9621e263879230abf7d5e2e0f8
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-01 22:58:55 -0300

    refactor: define Patient as entity

--- src/main/java/med/voll/api/model/patient/Patient.java
+++ src/main/java/med/voll/api/model/patient/Patient.java
@@ -1,4 +1,9 @@
 package med.voll.api.model.patient;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity(name = "Patient")
+@Table(name = "patients")
 public class Patient {
 }
