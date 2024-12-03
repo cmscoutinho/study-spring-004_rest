5f206e464e2fc38ff03943d305fce3e4ba721b58
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-02 22:46:06 -0300

    feat: include an autowired repository in Patient's controller

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,6 +1,8 @@
 package med.voll.api.controller;
 
 import med.voll.api.model.patient.PatientRegistryData;
+import med.voll.api.model.patient.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/patients")
 public class PatientController {
+
+    @Autowired
+    PatientRepository repository;
 
     @PostMapping
     public void register(@RequestBody PatientRegistryData data) {
