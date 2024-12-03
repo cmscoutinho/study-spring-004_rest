4d8bb8d7424767cc92ad6f6f32ba787f95ff9ca6
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-02 23:25:17 -0300

    refactor: change PatientRegistryData.address to NotNull annotation

--- src/main/java/med/voll/api/model/patient/PatientRegistryData.java
+++ src/main/java/med/voll/api/model/patient/PatientRegistryData.java
@@ -3,6 +3,7 @@ package med.voll.api.model.patient;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.model.address.AddressData;
 
@@ -22,7 +23,7 @@ public record PatientRegistryData(
         @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
         String cpf,
 
-        @NotBlank
+        @NotNull
         @Valid
         AddressData address) {
 }
