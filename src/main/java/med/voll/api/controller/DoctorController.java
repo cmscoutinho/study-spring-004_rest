bdf89c567f20f5f633fd9e3a76df4708154dcd2e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-03 16:35:03 -0300

    refactor: annotate list() method with GET mapping

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -6,10 +6,7 @@ import med.voll.api.model.doctor.DoctorRegistryData;
 import med.voll.api.model.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,7 @@ public class DoctorController {
         repository.save(new Doctor(data));
     };
 
+    @GetMapping
     public List<Doctor> list() {
 
     }
