0908dd2c16ca5ae0c08a271f86bca28a529ef406
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-02 22:46:51 -0300

    feat: annotate endpoint as transactional in Patient's controller

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import med.voll.api.model.patient.PatientRegistryData;
 import med.voll.api.model.patient.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class PatientController {
     PatientRepository repository;
 
     @PostMapping
+    @Transactional
     public void register(@RequestBody PatientRegistryData data) {
         System.out.println(data);
     }
