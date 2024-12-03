de9ea9ef79d9ba155967759d0c7bc4b7872f78f8
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-02 22:42:40 -0300

    feat: define constructor in Patient to get data from DTO

--- src/main/java/med/voll/api/model/patient/Patient.java
+++ src/main/java/med/voll/api/model/patient/Patient.java
@@ -2,8 +2,27 @@ package med.voll.api.model.patient;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import med.voll.api.model.address.AddressData;
 
 @Entity(name = "Patient")
 @Table(name = "patients")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Patient {
+
+    String name;
+    String email;
+    String phone;
+    String cpf;
+    AddressData address;
+
+    public Patient(PatientRegistryData data) {
+        this.name = data.name();
+        this.email = data.email();
+        this.phone = data.phone();
+        this.cpf = data.cpf();
+        this.address = data.address();
+    }
 }
