e361675b580a15f810228b3b918d68b9c8d5c4f4
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-02 22:45:10 -0300

    feat: annotate Patient's DTO with jakarta validation

--- src/main/java/med/voll/api/model/patient/PatientRegistryData.java
+++ src/main/java/med/voll/api/model/patient/PatientRegistryData.java
@@ -1,10 +1,29 @@
 package med.voll.api.model.patient;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.model.address.AddressData;
 
-public record PatientRegistryData(String name,
-                                  String email,
-                                  String phone,
-                                  String cpf,
-                                  AddressData address) {
+public record PatientRegistryData(
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String phone,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{11}")
+        String cpf,
+
+        @NotBlank
+        @Valid
+        AddressData address) {
 }
