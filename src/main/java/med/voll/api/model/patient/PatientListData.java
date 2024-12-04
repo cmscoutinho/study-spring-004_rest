401bf5ac01a9f702c49a0e2f62020def6700a0e9
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-04 18:36:50 -0300

    feat: make Patient's list DTO convert data from entity

--- src/main/java/med/voll/api/model/patient/PatientListData.java
+++ src/main/java/med/voll/api/model/patient/PatientListData.java
@@ -1,4 +1,8 @@
 package med.voll.api.model.patient;
 
-public record PatientListData() {
+public record PatientListData(String name, String email, String phone) {
+
+    public PatientListData(Patient patient) {
+        this(patient.getName(), patient.getEmail(), patient.getPhone());
+    }
 }
