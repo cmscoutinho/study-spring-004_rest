c634991d8ccd955802d49cf3807625e68500e375
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-04 18:38:20 -0300

    refactor: define default pagination for GET list

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -8,6 +8,7 @@ import med.voll.api.model.patient.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +20,7 @@ public class PatientController {
     private PatientRepository repository;
 
     @GetMapping
-    public Page<PatientListData> list(Pageable pagination) {
+    public Page<PatientListData> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
         return repository.findAll(pagination).map(PatientListData::new);
     }
 
