dfd270637206fa3e60ffe189c58dbb25f575d17a
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-05 14:31:53 -0300

    refactor: remove unused imports in DoctorController

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -8,12 +8,9 @@ import med.voll.api.model.doctor.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/doctors")
