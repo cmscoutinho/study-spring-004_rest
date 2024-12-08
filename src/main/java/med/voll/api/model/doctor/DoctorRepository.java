962436dbb7a3a6b82b6535dd030cd57928353d50
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-08 15:39:23 -0300

    feat: create method for retrieving only active doctors in repository

--- src/main/java/med/voll/api/model/doctor/DoctorRepository.java
+++ src/main/java/med/voll/api/model/doctor/DoctorRepository.java
@@ -1,6 +1,11 @@
 package med.voll.api.model.doctor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    public Page<Doctor> findByActiveTrue(Pageable pagination);
 }
