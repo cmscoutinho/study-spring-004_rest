aabeec56f5626e711b1e468f0efd933957610818
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-09 21:33:29 -0300

    refactor: include id field to patient's update record

--- src/main/java/med/voll/api/model/patient/PatientUpdateData.java
+++ src/main/java/med/voll/api/model/patient/PatientUpdateData.java
@@ -1,8 +1,12 @@
 package med.voll.api.model.patient;
 
+import jakarta.validation.constraints.NotNull;
 import med.voll.api.model.address.Address;
 
 public record PatientUpdateData(
+        @NotNull
+        Long id,
+
         String name,
         String phone,
         Address address
