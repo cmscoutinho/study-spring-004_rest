df9b14f9ff209075afe22e4cce0126d722aba24e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-09 21:36:24 -0300

    refactor: include Long id as a field in Patient's GET DTO

--- src/main/java/med/voll/api/model/patient/PatientListData.java
+++ src/main/java/med/voll/api/model/patient/PatientListData.java
@@ -1,8 +1,8 @@
 package med.voll.api.model.patient;
 
-public record PatientListData(String name, String email, String phone) {
+public record PatientListData(Long id, String name, String email, String phone) {
 
     public PatientListData(Patient patient) {
-        this(patient.getName(), patient.getEmail(), patient.getPhone());
+        this(patient.getId(), patient.getName(), patient.getEmail(), patient.getPhone());
     }
 }
