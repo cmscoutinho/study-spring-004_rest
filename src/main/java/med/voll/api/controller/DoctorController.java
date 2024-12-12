2735f26ccffa6dd32854bb6b61de1087e0834a87
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-12 16:28:24 -0300

    refactor: switch return in DELETE for Doctors to ResponseEntity

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,9 +37,10 @@ public class DoctorController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity delete(@PathVariable Long id) {
         //repository.deleteById(id);
         var doctor = repository.getReferenceById(id);
         doctor.delete();
+        return ResponseEntity.noContent().build();
     }
 }
