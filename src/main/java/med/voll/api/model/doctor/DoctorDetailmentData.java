3fe01c622952307ccd68bf06914e2b7eb69e8a74
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-12 16:48:12 -0300

    mend
    refactor: initialize Doctor's detailment DTO

--- src/main/java/med/voll/api/model/doctor/DoctorDetailmentData.java
+++ src/main/java/med/voll/api/model/doctor/DoctorDetailmentData.java
@@ -2,9 +2,9 @@ package med.voll.api.model.doctor;
 
 import med.voll.api.model.address.Address;
 
-public record DoctorDetailmentData(String name, String email, String crm, String phone, Speciality speciality, Address address) {
+public record DoctorDetailmentData(Long id, String name, String email, String crm, String phone, Speciality speciality, Address address) {
 
     public DoctorDetailmentData(Doctor doctor) {
-        this(doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getPhone(), doctor.getSpeciality(), doctor.getAddress());
+        this(doctor.getId(), doctor.getName(), doctor.getEmail(), doctor.getCrm(), doctor.getPhone(), doctor.getSpeciality(), doctor.getAddress());
     }
 }
