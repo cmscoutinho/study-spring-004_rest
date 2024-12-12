f24ebbe35c7cdc2edee2189e859c3c29eefbacd1
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-12 16:29:48 -0300

    refactor: switch return in GET for Doctors to ResponseEntity

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -23,9 +23,10 @@ public class DoctorController {
     };
 
     @GetMapping
-    public Page<DoctorListData> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
+    public ResponseEntity<Page<DoctorListData>> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pagination) {
         //return repository.findAll(pagination).map(DoctorListData::new);
-        return repository.findByActiveTrue(pagination).map(DoctorListData::new);
+        var page = repository.findByActiveTrue(pagination).map(DoctorListData::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
