9eba7162e202f0ab05077c03d7f2ff84bc9047aa
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-11 23:57:06 -0300

    feat: create method for filtering Patients by active in repo

--- src/main/java/med/voll/api/model/patient/PatientRepository.java
+++ src/main/java/med/voll/api/model/patient/PatientRepository.java
@@ -1,6 +1,11 @@
 package med.voll.api.model.patient;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByActiveTrue(Pageable pagination);
 }
