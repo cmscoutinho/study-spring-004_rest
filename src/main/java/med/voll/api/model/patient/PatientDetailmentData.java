0b504844020c0cd31e2e48013aebe12710155f64
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-15 22:49:12 -0300

    refactor: initialize Patient's detailment data from patient

--- src/main/java/med/voll/api/model/patient/PatientDetailmentData.java
+++ src/main/java/med/voll/api/model/patient/PatientDetailmentData.java
@@ -6,5 +6,5 @@ import med.voll.api.model.address.Address;
 public record PatientDetailmentData(Long id, String name, String email, String phone, String cpf, Address address) {
 
     public PatientDetailmentData(Patient patient) {
-    }
+        this(patient.getId(), patient.getName(), patient.getEmail(), patient.getPhone(), patient.getCpf(), patient.getAddress());    }
 }
