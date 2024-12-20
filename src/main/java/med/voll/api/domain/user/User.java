f74d53868bfc621bdc023c4bee38edf507d49709
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-19 22:10:25 -0300

    feat: implement class User

--- src/main/java/med/voll/api/domain/user/User.java
+++ src/main/java/med/voll/api/domain/user/User.java
@@ -1,4 +1,20 @@
 package med.voll.api.domain.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "User")
+@Table(name = "user")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String password;
 }
