dba528fa9195c0053ba6b348e903bba4c3a92497
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-22 21:05:36 -0300

    refactor: create UserRepository field in AuthenticationService

--- src/main/java/med/voll/api/domain/user/AuthenticationService.java
+++ src/main/java/med/voll/api/domain/user/AuthenticationService.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,6 +8,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
