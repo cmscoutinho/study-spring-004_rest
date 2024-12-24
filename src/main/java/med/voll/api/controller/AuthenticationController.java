e094f27357b5fe35949a7ae3b1aa8dfed38967c4
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-24 13:38:49 -0300

    refactor: set parameters for the login method

--- src/main/java/med/voll/api/controller/AuthenticationController.java
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -1,7 +1,9 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     @PostMapping
-    public ResponseEntity login() {
+    public ResponseEntity login(@RequestBody @Valid AuthenticationData data) {
 
     }
 }
