470deaa9b1a066c536295a4c954d4d4c51c588a2
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-24 13:44:25 -0300

    feat: create auth manager field in auth controller

--- src/main/java/med/voll/api/controller/AuthenticationController.java
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -2,7 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.user.AuthenticationData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager manager;
 
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid AuthenticationData data) {
