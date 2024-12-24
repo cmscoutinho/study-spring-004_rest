4a836ebae1016a6fc38badc76cf73aa8c5d1deda
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-23 22:07:40 -0300

    refactor: annotate SecurityConfigurations as @EnableWebSecurity

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfigurations {
 }
