924c20d4e074784e1ef3e3219a11db085fb12db3
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-24 13:46:40 -0300

    feat: generate token and authenticate it in auth controller

--- src/main/java/med/voll/api/controller/AuthenticationController.java
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import med.voll.api.domain.user.AuthenticationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class AuthenticationController {
 
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid AuthenticationData data) {
-
+        var token = new UsernamePasswordAuthenticationToken(data.uname(), data.upass());
+        var auth = manager.authenticate(token);
     }
 }
