40146109d3ad407f6fcbbe6e01a5e52de81af59e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-12-23 22:19:20 -0300

    refactor: annotate class AuthenticationController as @RestController

--- src/main/java/med/voll/api/controller/AuthenticationController.java
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -1,2 +1,7 @@
-package med.voll.api.controller;public class AuthenticationController {
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
 }
