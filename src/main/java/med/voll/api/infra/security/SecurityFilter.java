c74a14b69907ef7d1ce29b477d4749a147ec71f7
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-02 14:33:24 -0300

    refactor: remove test sout from 1st filter

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("REQUEST!");
         var token = retrieveToken(request);
         System.out.println(token);
 
