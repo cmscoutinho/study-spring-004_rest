2f07d374a1f14d01e72e6206e1e39099caca4515
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-07 22:38:30 -0300

    refactor: uncomment addFilterBefore()

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -29,7 +29,7 @@ public class SecurityConfigurations {
                     req.requestMatchers("/login").permitAll();
                     req.anyRequest().authenticated();
                 })
-                //.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
