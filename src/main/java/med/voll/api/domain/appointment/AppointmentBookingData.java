c7f2ff93810ccffc4aebc52cd5b5cb876952b079
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-09 20:55:34 -0300

    feat: set fields in AppointmentBookingData

--- src/main/java/med/voll/api/domain/appointment/AppointmentBookingData.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentBookingData.java
@@ -1,2 +1,18 @@
-package med.voll.api.domain.appointment;public record AppointmentBookingData() {
+package med.voll.api.domain.appointment;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record AppointmentBookingData(
+        Long idDoctor,
+
+        @NotNull
+        Long idPatient,
+
+        @NotNull
+        @Future
+        LocalDateTime date
+) {
 }
