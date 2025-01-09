ab90a9a68d13983128553e2e0894c36fbe6832a3
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-09 20:58:43 -0300

    feat: set fields on response DTO AppointmentDetailmentData

--- src/main/java/med/voll/api/controller/AppointmentDetailmentData.java
+++ src/main/java/med/voll/api/controller/AppointmentDetailmentData.java
@@ -1,2 +1,8 @@
-package med.voll.api.controller;public class AppointmentDetailmentData {
-}
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.appointment.AppointmentBookingData;
+
+import java.time.LocalDateTime;
+
+public record AppointmentDetailmentData(Long id, Long idDoctor, LocalDateTime date) {}
