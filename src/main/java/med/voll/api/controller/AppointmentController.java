c28203cfcee2253359a923800cb169556293849b
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-08 23:31:24 -0300

    feat: create book() method for appointments

--- src/main/java/med/voll/api/controller/AppointmentController.java
+++ src/main/java/med/voll/api/controller/AppointmentController.java
@@ -1,10 +1,18 @@
 package med.voll.api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/appointment")
+@RequestMapping("/appointments")
 public class AppointmentController {
 
+    @PostMapping
+    @Transactional
+    public ResponseEntity book() {
+
+    }
 }
