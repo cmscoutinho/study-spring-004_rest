ca63acee60f91e71c120264c690ac0a5362e4bcb
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-10 14:51:30 -0300

    refactor: make all parameter in appointmentdetail null for testing

--- src/main/java/med/voll/api/controller/AppointmentController.java
+++ src/main/java/med/voll/api/controller/AppointmentController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.appointment.AppointmentBookingData;
+import med.voll.api.domain.appointment.AppointmentDetailmentData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
