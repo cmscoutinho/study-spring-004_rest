08070481dbaf2d569929c87b72f02394aead3216
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-10 14:54:24 -0300

    feat: include JPA and Lombok annotation in model class Appointment

--- src/main/java/med/voll/api/domain/appointment/Appointment.java
+++ src/main/java/med/voll/api/domain/appointment/Appointment.java
@@ -1,2 +1,17 @@
-package med.voll.api.domain.appointment;public class Appointment {
+package med.voll.api.domain.appointment;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "appointments")
+@Entity(name = "Appointment")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Appointment {
 }
