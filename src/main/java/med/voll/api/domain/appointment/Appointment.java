35431379d6dc7af2c0f86e1d8e93bf62422ee1cf
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-10 14:56:28 -0300

    feat: define all fields in model class Appointment

--- src/main/java/med/voll/api/domain/appointment/Appointment.java
+++ src/main/java/med/voll/api/domain/appointment/Appointment.java
@@ -1,11 +1,14 @@
 package med.voll.api.domain.appointment;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.domain.doctor.Doctor;
+import med.voll.api.domain.patient.Patient;
+
+import java.time.LocalDateTime;
 
 @Table(name = "appointments")
 @Entity(name = "Appointment")
@@ -14,4 +17,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    private LocalDateTime date;
 }
