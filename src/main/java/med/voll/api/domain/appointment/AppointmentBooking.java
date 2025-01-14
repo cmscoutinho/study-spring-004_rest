85a4f0c97ec57e0b15e2de1ad7b3ea9150eda240
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-14 14:26:06 -0300

    feat: create fields for both doctor and patient repositories in booking service

--- src/main/java/med/voll/api/domain/appointment/AppointmentBooking.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentBooking.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.appointment;
 
+import med.voll.api.domain.doctor.DoctorRepository;
+import med.voll.api.domain.patient.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,13 @@ public class AppointmentBooking {
 
     @Autowired
     private AppointmentRepository repository;
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
     public void book(AppointmentBookingData data) {
         var appointment = new Appointment();
         repository.save(appointment);
