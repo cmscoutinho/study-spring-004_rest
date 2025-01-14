0bb7c75c236ec4a5116d378f0bea6420241ecc59
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-14 14:21:27 -0300

    feat: include repository field in Service AppointmentBooking

--- src/main/java/med/voll/api/domain/appointment/AppointmentBooking.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentBooking.java
@@ -1,10 +1,13 @@
 package med.voll.api.domain.appointment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppointmentBooking {
 
+    @Autowired
+    private AppointmentRepository repository;
     public void book(AppointmentBookingData data) {
 
     }
