c83e30f056ba3012a52a4f9bbe1cb968e79ef7a6
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-17 13:32:36 -0300

    feat: create method in doctor repository for random selection

--- src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
+++ src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     public Page<Doctor> findByActiveTrue(Pageable pagination);
+
+    Doctor chooseRandomDoctorAvailableInDate(Speciality speciality, LocalDateTime date);
 }
