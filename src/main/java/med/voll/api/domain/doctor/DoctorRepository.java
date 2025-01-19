82e81ed7877fea73ea591b34ba76dbb136572dd9
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-18 23:35:32 -0300

    feat: create JPQL query to retrieve random doctor from the DB

--- src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
+++ src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
@@ -3,11 +3,21 @@ package med.voll.api.domain.doctor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
     public Page<Doctor> findByActiveTrue(Pageable pagination);
 
+    @Query("""
+            SELECT d FROM Doctor d
+            WHERE d.active = 1 AND d.speciality = :speciality AND d.id NOT in(
+                SELECT a.doctor.id FROM Appointment a
+                WHERE a.date = :date 
+            ) 
+            ORDER BY rand()
+            LIMIT 1
+            """)
     Doctor chooseRandomDoctorAvailableInDate(Speciality speciality, LocalDateTime date);
 }
