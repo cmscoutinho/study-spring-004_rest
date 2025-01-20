4d6fa760fde79234cb89ce8afc8f8671f983126e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-20 20:49:39 -0300

    refactor: switch idAppointment in cancellation DTO to NotNull

--- src/main/java/med/voll/api/domain/appointment/AppointmentCancellationData.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentCancellationData.java
@@ -1,9 +1,11 @@
 package med.voll.api.domain.appointment;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record AppointmentCancellationData(
 
+        @NotNull
         Long idAppointment,
 
         @NotBlank
