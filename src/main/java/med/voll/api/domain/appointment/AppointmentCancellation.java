27232f78399a9bb7b514fbba81ab39784e6fb72e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-19 22:30:50 -0300

    refactor: annotate AppointmentCancellation as a Spring Service

--- src/main/java/med/voll/api/domain/appointment/AppointmentCancellation.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentCancellation.java
@@ -1,2 +1,7 @@
-package med.voll.api.domain.appointment;public class AppointmentCancellation {
+package med.voll.api.domain.appointment;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentCancellation {
 }
