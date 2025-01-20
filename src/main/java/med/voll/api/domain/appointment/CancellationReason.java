7e5caee23881cd6e8ed124ec4c8413425cc59f68
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-20 20:48:34 -0300

    feat: create fields for cancellation reason

--- src/main/java/med/voll/api/domain/appointment/CancellationReason.java
+++ src/main/java/med/voll/api/domain/appointment/CancellationReason.java
@@ -1,2 +1,7 @@
-package med.voll.api.domain.appointment;public enum CancellationReason {
+package med.voll.api.domain.appointment;
+
+public enum CancellationReason {
+    PATIENT_GAVE_UP,
+    DOCTOR_CANCELLED,
+    OTHER
 }
