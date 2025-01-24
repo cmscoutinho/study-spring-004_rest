27866190ff9293daf9825726d04eda462bf70e5e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-01-24 17:38:22 -0300

    feat: check if appointment exists before cancelling

--- src/main/java/med/voll/api/domain/appointment/AppointmentCancellation.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentCancellation.java
@@ -1,11 +1,14 @@
 package med.voll.api.domain.appointment;
 
+import med.voll.api.domain.ValidationException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppointmentCancellation {
 
     public void cancel(AppointmentCancellationData data) {
-
+        if (!appointmentRepository.existsById(data.idConsulta())) {
+            throw new ValidationException("The given appointment id doesn't exist!");
+        }
     }
 }
