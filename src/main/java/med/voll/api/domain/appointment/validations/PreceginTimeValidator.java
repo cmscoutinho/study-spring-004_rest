7919047430dce1cd7716500d703aedb4337eb1dc
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-05 19:16:45 -0300

    feat: implement validation for minimum of 30 minutes between booking and appointment

--- src/main/java/med/voll/api/domain/appointment/validations/PreceginTimeValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/PreceginTimeValidator.java
@@ -1,2 +1,20 @@
-package med.voll.api.domain.appointment.validations;public class PreceginTimeValidator {
+package med.voll.api.domain.appointment.validations;
+
+import med.voll.api.domain.ValidationException;
+import med.voll.api.domain.appointment.AppointmentBookingData;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class PreceginTimeValidator {
+
+    public void validate(AppointmentBookingData data) {
+        var appointmentDate = data.date();
+        var now = LocalDateTime.now();
+        var minuteDifference = Duration.between(now, appointmentDate).toMinutes();
+
+        if (minuteDifference < 30) {
+            throw new ValidationException("The appointment must booked at least 30 minutes before.");
+        }
+    }
 }
