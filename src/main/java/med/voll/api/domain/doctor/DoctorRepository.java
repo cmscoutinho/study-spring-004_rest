e6336630fbca75be7cceedf339771216818f9739
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-05 19:26:44 -0300

    feat: create derived query in doctor's repository to check active status

--- src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
+++ src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
@@ -1,14 +1,11 @@
 package med.voll.api.domain.doctor;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-    public Page<Doctor> findByActiveTrue(Pageable pagination);
 
     @Query("""
             SELECT d FROM Doctor d
@@ -20,4 +17,11 @@ public interface DoctorRepository extends JpaRepository<Doctor, Long> {
             LIMIT 1
             """)
     Doctor chooseRandomDoctorAvailableInDate(Speciality speciality, LocalDateTime date);
+
+    @Query("""
+            SELECT m.active 
+            FROM Doctor m
+            WHERE m.id = :id
+            """)
+    boolean findActiveById(Long id);
 }
