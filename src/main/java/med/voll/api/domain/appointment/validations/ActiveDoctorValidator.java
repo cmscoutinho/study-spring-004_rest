282b494b239ab8682201f56ff63f94339505ad09
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-05 19:24:08 -0300

    feat: implement active doctor validation

--- src/main/java/med/voll/api/domain/appointment/validations/ActiveDoctorValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/ActiveDoctorValidator.java
@@ -1,4 +1,23 @@
 package med.voll.api.domain.appointment.validations;
 
+import med.voll.api.domain.ValidationException;
+import med.voll.api.domain.appointment.AppointmentBookingData;
+import med.voll.api.domain.doctor.DoctorRepository;
+
 public class ActiveDoctorValidator {
+
+    private DoctorRepository repository;
+
+    public void validate(AppointmentBookingData data) {
+        var doctor = data.idDoctor();
+
+        if (doctor == null) {
+            return;
+        }
+
+        boolean isDoctorActive = repository.findActiveById(doctor);
+        if (!isDoctorActive) {
+            throw new ValidationException("The given doctor is not active!");
+        }
+    }
 }
