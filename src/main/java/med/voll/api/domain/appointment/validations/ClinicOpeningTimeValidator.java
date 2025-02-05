421508a14a00922bb5fd8cfaf4fd9cd315e9d5a1
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-04 22:27:16 -0300

    feat: implement method for valid appointment time verification

--- src/main/java/med/voll/api/domain/appointment/validations/ClinicOpeningTimeValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/ClinicOpeningTimeValidator.java
@@ -1,10 +1,22 @@
 package med.voll.api.domain.appointment.validations;
 
+import med.voll.api.domain.ValidationException;
 import med.voll.api.domain.appointment.AppointmentBookingData;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 
 public class ClinicOpeningTimeValidator {
 
     public void validate(AppointmentBookingData data) {
+        var appointmentDate = data.date();
 
+        var isSunday = appointmentDate.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var isBeforeOpening = appointmentDate.getHour() < 7;
+        var isAfterClosing = appointmentDate.getHour() > 18;
+
+        if(isSunday || isBeforeOpening || isAfterClosing) {
+            throw new ValidationException("Appointment outside the clinic's operating hours.")
+        }
     }
 }
