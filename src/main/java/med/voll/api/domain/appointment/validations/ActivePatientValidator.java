b56b68e631d99d242c550f7551831b5a27288731
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-06 19:33:58 -0300

    feat: create class to check if chosen patient is active

---
+++ src/main/java/med/voll/api/domain/appointment/validations/ActivePatientValidator.java
@@ -0,0 +1,4 @@
+package med.voll.api.domain.appointment.validations;
+
+public class ActivePatientValidator {
+}
--- src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
+++ src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
@@ -1,11 +1,14 @@
 package med.voll.api.domain.doctor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    public Page<Doctor> findByActiveTrue(Pageable pagination);
 
     @Query("""
             SELECT d FROM Doctor d
