9bdfa6edc8f72c8cd6dd77d1ee6ca8e1c394ea97
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-09 13:50:16 -0300

    feat: implement validation for appointment at conflicting time

--- src/main/java/med/voll/api/domain/appointment/validations/DoctorWithAnotherAppointmentAtSameTimeValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/DoctorWithAnotherAppointmentAtSameTimeValidator.java
@@ -1,8 +1,17 @@
 package med.voll.api.domain.appointment.validations;
 
+import med.voll.api.domain.ValidationException;
+import med.voll.api.domain.appointment.AppointmentBookingData;
 import med.voll.api.domain.appointment.AppointmentRepository;
 
 public class DoctorWithAnotherAppointmentAtSameTimeValidator {
 
     private AppointmentRepository repository;
+
+    public void validate(AppointmentBookingData data) {
+        var doctorHasAnotherAppointmentAtSameTime = repository.existsByDoctorIdAndDate(data.idDoctor(), data.date());
+        if(doctorHasAnotherAppointmentAtSameTime) {
+            throw new ValidationException("Doctor already has another appointment at the same time!");
+        }
+    }
 }
