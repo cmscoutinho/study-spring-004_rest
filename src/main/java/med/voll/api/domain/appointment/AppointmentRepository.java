0521a03b60aee689815ed26cbe6ff82322b31fe7
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-09 13:51:13 -0300

    feat: create validation method in appointment repository

--- src/main/java/med/voll/api/domain/appointment/AppointmentRepository.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentRepository.java
@@ -2,5 +2,8 @@ package med.voll.api.domain.appointment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    Boolean existsByDoctorIdAndDate(Long doctorId, LocalDateTime date);
 }
