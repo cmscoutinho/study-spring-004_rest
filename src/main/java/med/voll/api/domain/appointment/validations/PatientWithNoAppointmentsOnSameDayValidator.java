0384cd30c976730dbcc35b551fd57ac0d9e019de
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-11 10:15:03 -0300

    feat: implement validator for patient with appointment at the same day

--- src/main/java/med/voll/api/domain/appointment/validations/PatientWithNoAppointmentsOnSameDayValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/PatientWithNoAppointmentsOnSameDayValidator.java
@@ -1,4 +1,19 @@
 package med.voll.api.domain.appointment.validations;
 
+import med.voll.api.domain.ValidationException;
+import med.voll.api.domain.appointment.AppointmentBookingData;
+import med.voll.api.domain.appointment.AppointmentRepository;
+
 public class PatientWithNoAppointmentsOnSameDayValidator {
+
+    AppointmentRepository repository;
+
+    public void validate(AppointmentBookingData data) {
+        var startTime = data.date().withHour(7);
+        var finalTime = data.date().withHour(18);
+        var appointmentExists = repository.existsByPatientIdAndDateBetween(data.idPatient(), startTime, finalTime);
+        if(appointmentExists) {
+            throw new ValidationException("Patient already has another appointment at the same day!");
+        }
+    }
 }
