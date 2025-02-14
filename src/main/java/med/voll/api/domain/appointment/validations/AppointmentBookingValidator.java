49be35c6674f87476ff076bedbfdda49814c8e40
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-14 20:58:24 -0300

    feat: create validating method to be implemented by all validators

--- src/main/java/med/voll/api/domain/appointment/validations/AppointmentBookingValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/AppointmentBookingValidator.java
@@ -1,4 +1,8 @@
 package med.voll.api.domain.appointment.validations;
 
+import med.voll.api.domain.appointment.AppointmentBookingData;
+
 public interface AppointmentBookingValidator {
+
+    void validate(AppointmentBookingData data);
 }
