8cba73edab0837b7a7df254a76b6febea90d056e
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-18 23:18:19 -0300

    feat: create constructor in Appointment DTO

--- src/main/java/med/voll/api/domain/appointment/AppointmentDetailmentData.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentDetailmentData.java
@@ -5,4 +5,7 @@ import med.voll.api.domain.appointment.AppointmentBookingData;
 
 import java.time.LocalDateTime;
 
-public record AppointmentDetailmentData(Long id, Long idDoctor, Long idPatient, LocalDateTime date) {}
+public record AppointmentDetailmentData(Long id, Long idDoctor, Long idPatient, LocalDateTime date) {
+    public AppointmentDetailmentData(Appointment appointment) {
+        this(appointment.getId(), appointment.getDoctor().getId(), appointment.getPatient().getId(), appointment.getDate());    }
+}
