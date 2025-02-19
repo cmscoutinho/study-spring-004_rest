1a3baf781ee6f47e096d79e9a916a56b7b476dbe
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-19 13:39:24 -0300

    refactor: remove unused imports

--- src/main/java/med/voll/api/domain/appointment/AppointmentDetailmentData.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentDetailmentData.java
@@ -1,8 +1,5 @@
 package med.voll.api.domain.appointment;
 
-import jakarta.validation.Valid;
-import med.voll.api.domain.appointment.AppointmentBookingData;
-
 import java.time.LocalDateTime;
 
 public record AppointmentDetailmentData(Long id, Long idDoctor, Long idPatient, LocalDateTime date) {
