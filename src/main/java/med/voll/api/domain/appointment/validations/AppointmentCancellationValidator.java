2a003bc1918501956b3f4c2efcfbee85d54a6b31
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-02-24 22:52:39 -0300

    feat: create validate() in cancellation validator interface

--- src/main/java/med/voll/api/domain/appointment/validations/AppointmentCancellationValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/AppointmentCancellationValidator.java
@@ -1,4 +1,7 @@
 package med.voll.api.domain.appointment.validations;
 
+import med.voll.api.domain.appointment.AppointmentCancellationData;
+
 public interface AppointmentCancellationValidator {
+    void validate(AppointmentCancellationData data);
 }
