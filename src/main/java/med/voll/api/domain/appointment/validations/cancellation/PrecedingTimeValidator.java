90020a6988dfee9e6d27963bef04d6d5e574335b
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-03-01 23:38:46 -0300

    feat: implement validate() method for appointment cancellation

--- src/main/java/med/voll/api/domain/appointment/validations/cancellation/PrecedingTimeValidator.java
+++ src/main/java/med/voll/api/domain/appointment/validations/cancellation/PrecedingTimeValidator.java
@@ -1,11 +1,21 @@
 package med.voll.api.domain.appointment.validations.cancellation;
 
+import med.voll.api.domain.ValidationException;
 import med.voll.api.domain.appointment.AppointmentCancellationData;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class PrecedingTimeValidator implements AppointmentCancellationValidator{
 
     @Override
     public void validate(AppointmentCancellationData data) {
+        var appointment = repository.getReferenceById(data.idAppointment());
+        var now = LocalDateTime.now();
+        var hourDifference = Duration.between(now, appointment.getData()).toHours();
 
+        if (hourDifference < 24) {
+            throw new ValidationException("Appointments might be cancelled only up to 24 hours before!");
+        }
     }
 }
