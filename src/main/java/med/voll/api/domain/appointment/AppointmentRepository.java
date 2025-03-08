dd41e9b0dbf7af83f79bd2c4498d338c81bed2e6
Claudio Coutinho <ccoutinho.ti@gmail.com>
2025-03-07 23:29:40 -0300

    refactor: fix wrong method name in appointment repository

--- src/main/java/med/voll/api/domain/appointment/AppointmentRepository.java
+++ src/main/java/med/voll/api/domain/appointment/AppointmentRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-    boolean existsByDoctorIdAndDateAndReasonCancellationIsNull(Long idDoctor, LocalDateTime date);
+    boolean existsByDoctorIdAndDateAndCancellationReasonIsNull(Long idDoctor, LocalDateTime date);
 
     boolean existsByPatientIdAndDateBetween(Long idPatient, LocalDateTime startTime, LocalDateTime finalTime);
 }
